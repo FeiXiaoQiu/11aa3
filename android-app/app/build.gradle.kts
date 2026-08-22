5d66379586fafa7cc87cfbe5dda976708ee975fd
Merge Tool <merge@local>
2026-08-22 06:10:47 +0000

    chore: versionName 同步 versionCode 为 1.5.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 150
-        versionName = "Beta-1.4.10"
+        versionName = "Beta-1.5.0"
     }
 
     signingConfigs {
